d0e3203f00ac0e2345f2c6114f0f4b3eac37a7a4
shy_l <shy_l@DESKTOP-UQAN5FL>
2019-12-02 17:30:11 +0900

    1202 / 크롤링 업뎃중 / 이수현

---
+++ src/util/Crawling/service/impl/NewsCrawlingServiceImpl.java
@@ -0,0 +1,53 @@
+package util.Crawling.service.impl;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import util.Crawling.service.face.NewsCrawlingService;
+import util.Crawling.dao.face.NewsCrawlingDao;
+import util.Crawling.dao.impl.NewsClawlingDaoImpl;
+import util.Crawling.dto.News;
+
+public class NewsCrawlingServiceImpl implements NewsCrawlingService{
+//	private static String URL = "https://estate.nate.com/cp/news/research_list.asp?";
+	private static String URL = "http://www.donga.com/news/List/Economy/RE?";
+	
+	private NewsCrawlingDao newsDao = new NewsClawlingDaoImpl();
+	@Override
+	public List getCrawlingList() throws IOException {
+		// 1. Document를 가져온다
+		Document doc = Jsoup.connect(URL + newsDao.getParameter(21)).get();
+
+		// 2. 목록을 가져온다
+		//		System.out.println(""+ doc.toString());
+		Elements elements = doc.select(".articleList div a");
+
+		// 3. 목록(배열)에서 정보를 가져온다.
+		int idx = 0;
+
+		List list = new ArrayList();
+		for(Element element : elements) {
+			News news = new News();
+//			System.out.println(++idx + " : " + element.text());
+//			System.out.println(++idx + " : " + element.text());
+//			System.out.println("======================================");
+			news.setAtag(element.toString());
+//			System.out.println("daotest : " + news);
+			list.add(news);
+		}
+
+//		System.out.println("service list : " + list);
+		return list;
+
+	}
+
+	
+
+	
+}
